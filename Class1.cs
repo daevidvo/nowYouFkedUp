db2fbc3ae7906a3b080a307064d122c88631a1a1
daevidvo <itsdavidvo@gmail.com>
2024-01-16 21:02:37 -0800

    added more initial files

--- Class1.cs
+++ Class1.cs
@@ -1,4 +1,6 @@
 ﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,9 +9,31 @@ using System.Threading.Tasks;
 
 namespace nowYouFkedUp
 {
-    [BepInEx()]
+    [BepInPlugin(modGUID, modName, modVersion)]
     public class modBase : BaseUnityPlugin
     {
+        private const string modGUID = "daevidLethalMod";
+        private const string modName = "Now You Fked Up";
+        private const string modVersion = "1.0.0";
 
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static modBase Instance;
+
+        internal ManualLogSource log;
+
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            log = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            log.LogInfo("test mod");
+            
+            harmony.PatchAll(typeof(modBase));
+        }
     }
 }
