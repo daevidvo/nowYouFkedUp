cef63bb9c22ef6d253bd6bda23666cbf84c56628
daevidvo <itsdavidvo@gmail.com>
2024-01-16 20:35:16 -0800

    addedw main files

---
+++ Class1.cs
@@ -0,0 +1,15 @@
+﻿using BepInEx;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nowYouFkedUp
+{
+    [BepInEx()]
+    public class modBase : BaseUnityPlugin
+    {
+
+    }
+}
